8e4d370e7b78a146a082c18ba8f8f9bb20938245
Jasper van der Meer <jaspervdmeer@gmail.com>
2026-02-08 22:17:57 +0100

    Updated composer.json to standardize "Laravel" keyword capitalization. Oh and I updated like 20 packages.

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -8,10 +8,6 @@
   ),
   'laravel/reverb' => 
   array (
-    'aliases' => 
-    array (
-      'Output' => 'Laravel\\Reverb\\Output',
-    ),
     'providers' => 
     array (
       0 => 'Laravel\\Reverb\\ApplicationManagerServiceProvider',
